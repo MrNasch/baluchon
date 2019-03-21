c790eea5f447e326362ea1f5884e5bd8cd1525b5
Nasch <nasch@mac-mini-de-nasch.local>
2019-03-21 11:08:55 +0100

    Initial Commit

---
+++ Baluchon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Baluchon
+//
+//  Created by Nasch on 21/03/2019.
+//  Copyright © 2019 Nasch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
