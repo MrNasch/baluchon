8397ea3787317b6b98df7be824ef2fb62943a2e6
Nasch <nasch@Mini-de-Nasch.home>
2019-03-28 13:59:06 +0100

    added weather and change services

--- Model/ChangeModelService.swift
+++ Model/ChangeModelService.swift
@@ -9,10 +9,48 @@
 import Foundation
 
 class ChangeService {
-    private static let changeUrl = URL(string: "http://data.fixer.io/api/latest?access_key=c29967643b875302f204ce0268dd4343")!
+    // chared instance
+    static var shared = ChangeService()
+    private init() {}
+    // Url of the API
+    private let changeUrl = URL(string: "http://data.fixer.io/api/latest?access_key=c29967643b875302f204ce0268dd4343")!
     
     
+    // Create Session
+    private var changeSession = URLSession(configuration: .default)
     
+    init(changeSession: URLSession) {
+        self.changeSession = changeSession
+    }
+    private var task: URLSessionDataTask?
     
-    
+    // create func that get change rates from Fixer.io
+    func getChange(callback: @escaping (Bool, Change?) -> Void?) {
+        // cancel
+        task?.cancel()
+        // Task creation
+        task = changeSession.dataTask(with: changeUrl) { (data, response, error) in
+            DispatchQueue.main.async {
+                // Check for data
+                guard let data = data, error == nil else {
+                    callback(false, nil)
+                    return
+                }
+                //check response
+                guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                    callback(false, nil)
+                    return
+                }
+                // Check decoder
+                let decoder = JSONDecoder()
+                guard let changeRate = try? decoder.decode(Change.self, from: data) else {
+                        callback(false, nil)
+                        return
+                }
+                // succes
+                callback(true, changeRate)
+            }
+        }
+        task?.resume()
+    }
 }
--- Model/WeatherService.swift
+++ Model/WeatherService.swift
@@ -8,8 +8,50 @@
 
 import Foundation
 class WeatherService {
-    private static let weatherUrl = URL(string: "http://api.openweathermap.org/data/2.5/group?id=2796584,5128638&APPID=3066b37bb5e88c5fff0962e868a87a39&units=metric&lang=fr")!
+    //Instance
+    static var shared = WeatherService()
+    private init() {}
     
+    // API URL
+    private let weatherUrl = URL(string: "http://api.openweathermap.org/data/2.5/group?id=2796584,5128638&APPID=3066b37bb5e88c5fff0962e868a87a39&units=metric&lang=fr")!
     
+    //create session
+    private var weatherSession = URLSession(configuration: .default)
+    
+    init(weatherSession: URLSession) {
+        self.weatherSession = weatherSession
+    }
+    private var task: URLSessionDataTask?
+    
+    //create func that get weather from openweathermap
+    func getWeather(callback: @escaping (Bool, Weather?) ->Void?) {
+        //cancel
+        task?.cancel()
+        //task creation
+        task = weatherSession.dataTask(with: weatherUrl) { (data, response, error) in
+            DispatchQueue.main.async {
+                // Check for data
+                guard let data = data, error == nil else {
+                    callback(false, nil)
+                    return
+                }
+                //check response
+                guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                    callback(false, nil)
+                    return
+                }
+                // Check decoder
+                let decoder = JSONDecoder()
+                guard let weather = try? decoder.decode(Weather.self, from: data) else {
+                    callback(false, nil)
+                    return
+                }
+                // succes
+                callback(true, weather)
+            }
+        }
+        task?.resume()
+    }
 }
+
 
